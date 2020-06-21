dec60fa57dc3de3da0d247c3fb0f9a8d28e31c36
lucasmrpinho <lucaspinho101@gmail.com>
2020-06-20 23:54:15 -0300

    Página completa

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
+const modal = document.querySelector('.modal')
 const modalOverlay = document.querySelector('.modal_overlay')
 const cards = document.querySelectorAll('.card')
 
+
 for(let card of cards){
     card.addEventListener("click",function(){
+        cardId = card.getAttribute("id")
         modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src =`https://rocketseat.com.br/${cardId}`
     })
 }
 
 document.querySelector('.modal_close').addEventListener("click", function(){
     modalOverlay.classList.remove("active")
+    modal.classList.remove("maximized")
 })
 
-
+document.querySelector('.modal_max').addEventListener('click', () => {
+    if (modal.classList == "modal"){
+        modal.classList.add("maximized")
+    }
+    else{
+        modal.classList.remove("maximized")
+    }
+})
